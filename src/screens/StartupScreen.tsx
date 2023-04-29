e44372e79cd79d1842855b94fb4fe96d1fab081c
Anas <17gcs23@meaec.edu.in>
2023-04-29 17:12:48 +0530

    Project structure, resolved #1

---
+++ src/screens/StartupScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StartupScreen = () => {
+  return (
+    <View>
+      <Text>StartupScreen</Text>
+    </View>
+  );
+};
+
+export default StartupScreen;
+
+const styles = StyleSheet.create({});
